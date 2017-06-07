a6997f0f769e4edef7e1018225427e06ef369d9e
Steve Kenworthy <steveyken@gmail.com>
2017-06-07 14:52:57 +0800

    Fix typo in environment variable.

--- pilot/lib/pilot/options.rb
+++ pilot/lib/pilot/options.rb
@@ -18,7 +18,7 @@ module Pilot
                                      env_name: "PILOT_APP_IDENTIFIER",
                                      description: "The bundle identifier of the app to upload or manage testers (optional)",
                                      optional: true,
-                                     default_value: ENV["TESTFLIGHT_APP_IDENTITIFER"] || CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)),
+                                     default_value: ENV["TESTFLIGHT_APP_IDENTIFER"] || CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)),
         FastlaneCore::ConfigItem.new(key: :app_platform,
                                      short_option: "-m",
                                      env_name: "PILOT_PLATFORM",
